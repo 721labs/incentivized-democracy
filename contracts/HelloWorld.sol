4b58330fd4ddca14b50c68a80e8eb6c042e1ad2e
Will Holley <will@willholley.com>
2022-01-09 20:40:03 -0500

    Adds contract logic & tests

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,7 +3,106 @@
 pragma solidity ^0.8.10;
 
 contract HelloWorld {
-  constructor(){
-    require(true, "");
+  mapping(address => uint256) public voteAllowance;
+
+  // For example sake, voting is on a single proposition.
+  mapping(uint8 => mapping(address => uint256)) public votes;
+
+  // Deployer governs the election
+  address public governor;
+
+  // The citizens
+  address private _bob;
+  address private _alice;
+  address private _sue;
+
+  constructor(
+    address bob,
+    address alice,
+    address sue
+  ) {
+    // Assign deployer as the governor of the contract.
+    governor = msg.sender;
+
+    // Add the citizens' addresses
+    _bob = bob;
+    _alice = alice;
+    _sue = sue;
+
+    // Give each of our example "citizens" 10 votes to start.
+    voteAllowance[bob] = 10;
+    voteAllowance[alice] = 10;
+    voteAllowance[sue] = 10;
+
+    // Instantiate vote count
+    votes[1][bob] = 0;
+    votes[1][sue] = 0;
+    votes[1][alice] = 0;
+    votes[2][bob] = 0;
+    votes[2][sue] = 0;
+    votes[2][alice] = 0;
+  }
+
+  /// @notice Casting votes
+  /// @param count How many votes does this citizen want to use?
+  function vote(uint256 count, uint8 option) public {
+    require(count <= voteAllowance[msg.sender], "Not enough votes");
+    require(option == 1 || option == 2, "Bad option");
+
+    // In quadratic voting, the square root of the number of votes cast are applied;
+    // this enforces an exponential marginal cost on power concentration and acts to
+    // counterbalance our vote award / dilution system.
+    if (count == 1) {
+      votes[option][msg.sender] += 1;
+    } else if (count == 4) {
+      votes[option][msg.sender] += 2;
+    } else if (count == 9) {
+      votes[option][msg.sender] += 3;
+    } else {
+      require(false, "Count must be quadratic");
+    }
+
+    // Subtract votes from voter.
+    voteAllowance[msg.sender] -= count;
+  }
+
+  /// @notice Determine how many votes each voter should have in the next election.
+  /// If citizen cast a vote, provide an additional vote next time.
+  /// If citizen didn't cast a vote, remove a vote next time.
+  /// Future: This could be expanded to add/subtrack proportional to the number of votes cast to incentivize
+  /// people to cast all of their votes.
+  /// @param voter Voter address to check.
+  function setNextVoteAllowance(address voter) internal {
+    if (votes[1][voter] == 0 && votes[2][voter] == 0) {
+      voteAllowance[voter] = 9;
+    } else {
+      voteAllowance[voter] = 11;
+    }
+  }
+
+  /// @notice Counts number of votes for a given option id.
+  /// @param option option id.
+  /// @return vote count
+  function getVoteCount(uint8 option) public view returns (uint256) {
+    require(option == 1 || option == 2, "Bad option");
+    return votes[option][_bob] + votes[option][_sue] + votes[option][_alice];
+  }
+
+  /// @return ID of winning option
+  function closeVoting() public returns (uint256) {
+    // Only the governor can close elections
+    require(msg.sender == governor, "Governor only");
+
+    // Set vote allowance for next election
+    setNextVoteAllowance(_bob);
+    setNextVoteAllowance(_sue);
+    setNextVoteAllowance(_alice);
+
+    // Return the winning option
+    if (getVoteCount(1) > getVoteCount(2)) {
+      return 1;
+    } else {
+      return 2;
+    }
   }
 }
--- tests/HelloWorld.ts
+++ tests/HelloWorld.ts
@@ -1,18 +1,106 @@
 import { Contract } from "@ethersproject/contracts";
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
 
 describe("HelloWorld.sol", async () => {
   let contract: Contract;
 
-  before(async function () {
-    const factory = await ethers.getContractFactory("HelloWorld");
+  let governor: Contract;
 
-    contract = await factory.deploy();
+  let bob: Contract;
+  let bobAddress: string;
+
+  let sue: Contract;
+  let sueAddress: string;
+
+  let alice: Contract;
+  let aliceAddress: string;
+
+  const provider = hre.waffle.provider;
+  let snapshot: any;
+
+  async function snapshotEVM(): Promise<void> {
+    snapshot = await provider.send("evm_snapshot", []);
+  }
+
+  before(async function () {
+    const signers = await ethers.getSigners();
+
+    bobAddress = await signers[1].getAddress();
+    sueAddress = await signers[2].getAddress();
+    aliceAddress = await signers[3].getAddress();
+
+    const factory = await ethers.getContractFactory("HelloWorld");
+    contract = await factory.deploy(bobAddress, aliceAddress, sueAddress);
     await contract.deployed();
+
+    governor = contract.connect(signers[0]);
+    bob = contract.connect(signers[1]);
+    sue = contract.connect(signers[2]);
+    alice = contract.connect(signers[3]);
+
+    await snapshotEVM();
+  });
+
+  // Reset contract state
+  beforeEach(async function () {
+    await provider.send("evm_revert", [snapshot]);
+    await snapshotEVM();
   });
 
   it("deploys", async () => {
     expect(contract.address).to.not.be.null;
+  });
+
+  describe("Voting", async () => {
+    it("Does not allow casting more than allowance", async () => {
+      await expect(bob.vote(11, 1)).to.be.revertedWith("Not enough votes");
+    });
+
+    it("To simplify the example, vote counts must be quadratic", async () => {
+      await expect(bob.vote(2, 1)).to.be.revertedWith(
+        "Count must be quadratic"
+      );
+    });
+
+    it("Votes are counted quadratically", async () => {
+      // 1 vote on 1
+      await bob.vote(1, 1);
+      expect(await contract.getVoteCount(1)).to.equal(1);
+      expect(await contract.voteAllowance(bobAddress)).to.equal(9);
+
+      // 4 votes on 2
+      await bob.vote(4, 2); // 4 => 2;
+      expect(await contract.getVoteCount(2)).to.equal(2);
+      expect(await contract.voteAllowance(bobAddress)).to.equal(5);
+
+      // 3 votes on 2
+      await sue.vote(9, 2); // 9 => 3
+      expect(await contract.getVoteCount(2)).to.equal(5);
+    });
+  });
+
+  describe("Vote Allowance Determination", async () => {
+    it("Decreases future allowance by 1 if citizen didn't vote", async () => {
+      await bob.vote(1, 1);
+      await alice.vote(4, 2);
+
+      await governor.closeVoting();
+
+      expect(await contract.voteAllowance(sueAddress)).to.equal(9);
+      expect(await contract.voteAllowance(bobAddress)).to.not.equal(9);
+      expect(await contract.voteAllowance(aliceAddress)).to.not.equal(9);
+    });
+
+    it("Increases future allowance by 1 if citizen voted", async () => {
+      await bob.vote(1, 1);
+      await alice.vote(4, 2);
+
+      await governor.closeVoting();
+
+      expect(await contract.voteAllowance(sueAddress)).to.not.equal(11);
+      expect(await contract.voteAllowance(bobAddress)).to.equal(11);
+      expect(await contract.voteAllowance(aliceAddress)).to.equal(11);
+    });
   });
 });
